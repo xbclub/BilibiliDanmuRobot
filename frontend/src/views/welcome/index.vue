e4c33bfcac8304b22432f970fc98a6b9ebf8268b
yii <yii@neuedu.work>
2023-07-08 11:12:27 +0800

    前端把那破玩意分时段欢迎写完了，折磨死了

--- frontend/src/views/welcome/index.vue
+++ frontend/src/views/welcome/index.vue
@@ -62,15 +62,144 @@ const data = reactive({
       ]
     }],
     WelcomeBlacklistWide: [],
-    WelcomeBlacklist: []
-
+    WelcomeBlacklist: [],
+    InteractWordByTime: false,
+    WelcomeDanmuByTime: []
   }
+});
+const WelcomeDanmuByTimeTemplate = [{
+  Enabled: true,
+  Key: "earlymorning",
+  Random: true,
+  Danmu: [
+    "欢迎 {user}, 凌晨的问候~",
+    "欢迎 {user}, 早安，幸福满满",
+    "欢迎 {user}, 祝梦想成真",
+    "欢迎 {user}, 望快乐起步",
+    "欢迎 {user}, 望奋斗加油",
+    "欢迎 {user}, 祝充满力量",
+    "欢迎 {user}, 祝精神旺盛",
+    "欢迎 {user}, 望谦虚前行",
+    "欢迎 {user}, 祝青春无限",
+    "欢迎 {user}, 祝前程似锦"
+  ]
+},
+  {
+    Enabled: true,
+    Key: "morning",
+    Random: true,
+    Danmu: [
+      "欢迎 {user}, 早安，美好开始",
+      "欢迎 {user}, 早安，成功在望",
+      "欢迎 {user}, 早安，加油！",
+      "欢迎 {user}, 早安，勇往直前",
+      "欢迎 {user}, 早安，工作愉快",
+      "欢迎 {user}, 早安，眉开眼笑",
+      "欢迎 {user}, 早安，携手前行",
+      "欢迎 {user}, 早安，让生活更好",
+      "欢迎 {user}, 早安，热情满怀"
+    ]
+  },
+  {
+    Enabled: true,
+    Key: "latemorning",
+    Random: true,
+    Danmu: [
+      "欢迎 {user}, 上午好，奋斗有力",
+      "欢迎 {user}, 上午好，朝气蓬勃",
+      "欢迎 {user}, 上午好，加油向前",
+      "欢迎 {user}, 上午好，祝福无限",
+      "欢迎 {user}, 上午好，暖心问候",
+      "欢迎 {user}, 上午好，快乐工作",
+      "欢迎 {user}, 上午好，信心十足",
+      "欢迎 {user}, 上午好，开心一整天",
+      "欢迎 {user}, 上午好，精神满满",
+      "欢迎 {user}, 上午好，工作愉快"
+    ]
+  },
+  {
+    Enabled: true,
+    Key: "noon",
+    Random: true,
+    Danmu: [
+      "欢迎 {user}, 中午好，午餐愉快",
+      "欢迎 {user}, 中午好，美食充盈",
+      "欢迎 {user}, 中午好，快乐午时",
+      "欢迎 {user}, 中午好，记得午间小憩",
+      "欢迎 {user}, 中午好，吃好喝好",
+      "欢迎 {user}, 中午好，百般顺利"
+    ]
+  },
+  {
+    Enabled: true,
+    Key: "afternoon",
+    Random: true,
+    Danmu: [
+      "欢迎 {user}, 下午好，动力十足",
+      "欢迎 {user}, 下午好，活力满满",
+      "欢迎 {user}, 下午好，祝你成功",
+      "欢迎 {user}, 下午好，笑容满面",
+      "欢迎 {user}, 下午好，开心每时",
+      "欢迎 {user}, 下午好，工作顺利",
+      "欢迎 {user}, 下午好，美好心情",
+      "欢迎 {user}, 下午好，活力四射",
+      "欢迎 {user}, 下午好，幸福无限"
+    ]
+  },
+  {
+    Enabled: true,
+    Key: "night",
+    Random: true,
+    Danmu: [
+      "欢迎 {user}, 晚上好，祝福相伴",
+      "欢迎 {user}, 晚上好，幸福安康",
+      "欢迎 {user}, 晚上好，心情愉悦",
+      "欢迎 {user}, 晚上好，生活美满",
+      "欢迎 {user}, 晚上好，夜色温馨",
+      "欢迎 {user}, 晚上好，美梦成真",
+      "欢迎 {user}, 晚上好，美好的晚上",
+      "欢迎 {user}, 祝愿你今晚开心"
+    ]
+  },
+  {
+    Enabled: true,
+    Key: "midnight",
+    Random: true,
+    Danmu: [
+      "欢迎 {user}, 午夜好，月色清朗",
+      "欢迎 {user}, 午夜好，真诚问候",
+      "欢迎 {user}, 午夜好，祝福相随",
+      "欢迎 {user}, 午夜深沉，你不是独自一人",
+      "欢迎 {user}, 午夜好，静夜祝福",
+      "欢迎 {user}, 祝福你坚定向前",
+      "欢迎 {user}, 午夜沉寂，吉祥相随。",
+      "欢迎 {user}, 午夜好，还没休息?",
+      "欢迎 {user}, 拥抱午夜的美好"
+    ]
+  }
+];
+//凌晨 - Early morning   2:00--5:00
+// 早晨 - Morning   5:00--9:00
+// 上午 - Late morning / Mid-morning  9:00--11:00
+// 中午 - Noon  11:00--14:00
+// 下午 - Afternoon 14:00 -- 20:00
+// 晚上 - Evening / Night 20:00--00:00
+// 午夜 - Midnight 00:00 -- 2:00
+const WelcomeDanmuByTimeDescribe = reactive({
+  earlymorning: "凌晨2:00-5:00",
+  morning: "早晨5:00-9:00",
+  latemorning: "上午9:00-11:00",
+  noon: "中午11:00-14:00",
+  afternoon: "下午14:00-20:00",
+  night: "晚上20:00-00:00",
+  midnight: "午夜00:00-2:00"
 });
 import { ref } from "vue";
 import type { FormInstance, FormRules, TabsPaneContext } from "element-plus";
 import { ReadConfig, WriteConfig } from "../../../wailsjs/go/main/App";
 import { ElNotification } from "element-plus";
 import { Monitor, Start, Stop } from "../../../wailsjs/go/main/Program";
+// import { func } from "vue-types";
 
 const activeName = ref("first");
 
@@ -78,7 +207,10 @@ const handleClick = (tab: TabsPaneContext, event: Event) => {
   console.log(tab, event);
 };
 const addWelcomeDanmu = () => {
-  data.form.WelcomeDanmu.push("{user}");
+  data.form.WelcomeDanmu.unshift("{user}");
+};
+const addWelcomeDanmuByTime = (index) => {
+  data.form.WelcomeDanmuByTime[index].Danmu.unshift("{user}");
 };
 const addWelcomeBlacklist = () => {
   data.form.WelcomeBlacklist.push("");
@@ -91,6 +223,37 @@ const deleteWelcomeDanmu = (item: number) => {
   if (item !== -1) {
     data.form.WelcomeDanmu.splice(item, 1);
   }
+};
+const deleteWelcomeDanmuByTime = (index1,index2) => {
+  // const index = data.form.WelcomeDanmu.indexOf(item)
+  if (index1 !== -1 && index2 !== -1) {
+    data.form.WelcomeDanmuByTime[index1].Danmu.splice(index2, 1);
+  }
+};
+const initWelcomeDanmuByTime = () => {
+  if (data.form.WelcomeDanmuByTime == null){
+    data.form.WelcomeDanmuByTime = WelcomeDanmuByTimeTemplate;
+    return;
+  }
+  if (data.form.WelcomeDanmuByTime.length == 0) {
+    data.form.WelcomeDanmuByTime = WelcomeDanmuByTimeTemplate;
+    return;
+  }
+  if (data.form.WelcomeDanmuByTime.length < 7) {
+    for (let i = 0; i < WelcomeDanmuByTimeTemplate.length; i++) {
+      let x = true;
+      for (let a = 0; i < data.form.WelcomeDanmuByTime.length; i++) {
+        if (data.form.WelcomeDanmuByTime[a].Key === WelcomeDanmuByTimeTemplate[i].Key) {
+          x = false;
+          break;
+        }
+      }
+      if (x) {
+        data.form.WelcomeDanmuByTime.push(WelcomeDanmuByTimeTemplate[i]);
+      }
+    }
+  }
+  return;
 };
 const deleteWelcomeBlacklist = (item: number) => {
   // const index = data.form.WelcomeDanmu.indexOf(item)
@@ -112,6 +275,9 @@ const deleteFocusDanmu = (item: number) => {
   if (item !== -1) {
     data.form.FocusDanmu.splice(item, 1);
   }
+};
+const getWelcomeDanmuByTimeDescribe = (key) => {
+  return WelcomeDanmuByTimeDescribe[key];
 };
 
 function formatWelcomeString() {
@@ -241,7 +407,6 @@ const rules = reactive<FormRules>({
   ]
 });
 onMounted(() => {
-
   ReadConfig().then(res => {
     if (!res.Code) {
       console.log(res);
@@ -253,9 +418,9 @@ onMounted(() => {
     } else {
       data.form = res.Form;
       formatWelcomeString();
+      initWelcomeDanmuByTime();
     }
   });
-
 });
 </script>
 
@@ -288,23 +453,49 @@ onMounted(() => {
       </el-tab-pane>
 
       <el-tab-pane label="欢迎弹幕自定义" name="second">
+
         <el-form-item>
           <el-tag>Tips: 自定义欢迎语 {user}为用户昵称占位符，列表为随机列表</el-tag>
         </el-form-item>
-        <el-form-item>
+        <el-form-item label="时间段时间欢迎">
+          <el-switch v-model="data.form.InteractWordByTime" />
+        </el-form-item>
+        <el-form-item v-if="data.form.InteractWordByTime == false">
           <el-button type="primary" @click="addWelcomeDanmu">新增</el-button>
         </el-form-item>
         <el-form-item
+          v-if="data.form.InteractWordByTime == false"
           v-for="(items, index) in data.form.WelcomeDanmu"
           :label="'欢迎弹幕 ' + (index+1)"
         >
           <el-input v-model="data.form.WelcomeDanmu[index]" />
-          <el-button class="mt-2"
-                     @click="deleteWelcomeDanmu(index)"
+          <el-button class="mt-2" @click="deleteWelcomeDanmu(index)"
           >删除
-          </el-button
-          >
+          </el-button>
         </el-form-item>
+        <div v-if="data.form.InteractWordByTime == true" v-for="(items, index) in data.form.WelcomeDanmuByTime">
+          <el-text class="el-text--large" type="primary">{{getWelcomeDanmuByTimeDescribe(data.form.WelcomeDanmuByTime[index].Key)}}</el-text>
+          <el-form-item>
+            <el-button type="primary" @click="addWelcomeDanmuByTime(index)">新增</el-button>
+          </el-form-item>
+          <el-form-item label="启用">
+            <el-switch v-model="data.form.WelcomeDanmuByTime[index].Enabled" />
+          </el-form-item>
+          <el-form-item label="随机弹幕">
+            <el-switch v-model="data.form.WelcomeDanmuByTime[index].Random" />
+          </el-form-item>
+          <el-form-item
+            v-for="(items, index2) in data.form.WelcomeDanmuByTime[index].Danmu"
+            :label="'欢迎弹幕 ' + (index2+1)"
+          >
+            <el-input v-model="data.form.WelcomeDanmuByTime[index].Danmu[index2]" />
+            <el-button class="mt-2" @click="deleteWelcomeDanmuByTime(index,index2)"
+            >删除
+            </el-button>
+          </el-form-item>
+
+        </div>
+
       </el-tab-pane>
       <el-tab-pane label="指定人欢迎" name="third">
         <!--        <template>-->
@@ -374,7 +565,7 @@ onMounted(() => {
           </el-tag>
         </el-form-item>
         <el-form-item>
-          <el-tag>Tips2: 随即开关打开表示随机发送列表中的一条弹幕, 关闭则是顺序发送</el-tag>
+          <el-tag>Tips2: 随机开关打开表示随机发送列表中的一条弹幕, 关闭则是顺序发送</el-tag>
         </el-form-item>
         <el-form-item>
           <el-button
@@ -476,7 +667,7 @@ onMounted(() => {
     width="30%"
   >
     <el-form-item label="用户uid">
-      <el-input v-model.number="data.dialogdata.uid" :disabled = data.dialogAction></el-input>
+      <el-input v-model.number="data.dialogdata.uid" :disabled=data.dialogAction></el-input>
     </el-form-item>
     <el-form-item label="欢迎语">
       <el-input v-model="data.dialogdata.msg"></el-input>
